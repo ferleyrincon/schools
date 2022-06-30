b6056ea86e2587d0e2087e60692707a28b696641
Lali <lauracamiladiazdelgado@gmail.com>
2022-06-30 10:40:46 -0500

    mundo trabajo 5

--- questions/pages.py
+++ questions/pages.py
@@ -112,5 +112,6 @@ class thanks(Page):
 #                thanks]
 
 page_sequence = [consent, ingresar, sigue, mi_mundo , mi_mundo1 , mi_mundo2, mi_mundo3, mi_mundo4, 
-                 mundo_trabajo, mundo_trabajo1, mundo_trabajo2, mundo_trabajo3, mundo_trabajo4]
+                 mundo_trabajo, mundo_trabajo1, mundo_trabajo2, mundo_trabajo3, mundo_trabajo4,
+                 mundo_trabajo5]
 
