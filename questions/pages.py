dc4bb0781098a0599856bd6598781105552b1053
Lali <lauracamiladiazdelgado@gmail.com>
2022-06-29 20:42:11 -0500

    mimundo1

--- questions/pages.py
+++ questions/pages.py
@@ -111,5 +111,5 @@ class thanks(Page):
 #                mundo_formacion, mundo_formacion1, mundo_formacion2, mundo_formacion3, mundo_formacion4, mundo_formacion5, mundo_formacion5, mundo_formacion7,
 #                thanks]
 
-page_sequence = [sigue, mi_mundo3]
+page_sequence = [sigue, mi_mundo1 , mi_mundo3]
 
