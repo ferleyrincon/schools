7906909e3e6c16b45684b31c3fe9bc44db786e1f
Lali <lauracamiladiazdelgado@gmail.com>
2022-07-02 18:07:36 -0500

    Update pages.py

--- questions/pages.py
+++ questions/pages.py
@@ -115,7 +115,7 @@ class thanks(Page):
 
 #page_sequence = [consent , ingresar, sigue ,        mi_mundo , mi_mundo1 , mi_mundo2 , mi_mundo3 , mi_mundo4 , 
                  #mundo_trabajo , mundo_trabajo1 , mundo_trabajo2 , mundo_trabajo3 , mundo_trabajo4 ,
-                 #mundo_trabajo5 , mundo_trabajo6 , mundo_trabajo7 ,                  mundo_trabajo9 , 
+                 #mundo_trabajo5 , mundo_trabajo6 , mundo_trabajo7 , mundo_trabajo8 , mundo_trabajo9 , 
                  #mundo_trabajo10 , mundo_trabajo11 , mundo_trabajo12 , mundo_trabajo13 , mundo_trabajo14 , 
                  #mundo_formacion , mundo_formacion1 , mundo_formacion2 , mundo_formacion3 , mundo_formacion4 ,
                  #mundo_formacion5 , mundo_formacion6 , mundo_formacion7 , thanks]
