1bb8ca8888c950ee0b654db3bd0f50ae50c09c75
Jorge Saul <jorge.castillo1@correo.uis.edu.co>
2022-07-12 16:15:40 -0500

    Add table for signatures, scores and preference

--- questions/models.py
+++ questions/models.py
@@ -65,3 +65,17 @@ class Player(BasePlayer):
     mundo_formacion6 = models.StringField()
     mundo_formacion7 = models.StringField()
 
+    prueba_20000 = models.StringField();
+    prueba_15000 = models.StringField();
+    prueba_10000 = models.StringField();
+    prueba_5000 = models.StringField();
+
+    puntaje_20000 = models.IntegerField();
+    puntaje_15000 = models.IntegerField();
+    puntaje_10000 = models.IntegerField();
+    puntaje_5000 = models.IntegerField();
+
+    prefer_20000 = models.IntegerField();
+    prefer_15000 = models.IntegerField();
+    prefer_10000 = models.IntegerField();
+    prefer_5000 = models.IntegerField();
